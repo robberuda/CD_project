f2f1124e16fe10ec6d5d5f3798e1699a8f33753e
robberuda <robberuda@gmail.com>
2021-08-12 16:16:53 +0200

    update

---
+++ SetWorkspace.m
@@ -0,0 +1,21 @@
+%set workspace
+
+%% Setup Drone
+m = 0.2
+I = 0.1
+
+% sample time
+ts = 0.01
+
+% Initial States 
+Becc_0 = 0
+
+% Start position 
+XY_0 = [1;1]
+%body_rate_0 = [0;0;0]
+
+% Environment
+g = -9.81
+
+% Target position
+target = [2;2]
