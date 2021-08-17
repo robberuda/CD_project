525cd0d7dd307a10f27cd59bb53a5c1cfc561104
robberuda <robberuda@gmail.com>
2021-08-17 11:22:04 +0200

    add second simulink

--- SetWorkspace.m
+++ SetWorkspace.m
@@ -1,21 +1,40 @@
 %set workspace
 
 %% Setup Drone
-m = 0.2
-I = 0.1
+m = 0.2;
+I = 0.1;
 
 % sample time
-ts = 0.01
+ts = 0.01;
 
 % Initial States 
-Becc_0 = 0
+Becc_0 = 0;
 
 % Start position 
-XY_0 = [1;1]
+XY_0 = [1;1];
 %body_rate_0 = [0;0;0]
 
 % Environment
-g = -9.81
+g = -9.81;
+
+
+WayPts = [[0,0,1];
+          [0,-1,2];
+          [3,-4,6];
+          [-1,-4,10];
+          [2,-1,14];
+          [1,-5.5,18];
+          [0,-1,22];
+          ]
+
 
 % Target position
-target = [2;2]
+z_target = 2;
+y_target = 2;
+
+%liftoff or takeoff time
+TOFtime = 1;
+
+
+
+
