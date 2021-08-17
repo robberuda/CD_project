2e78ac9d7a2cba826cedc1d52a263d3cb897b2a5
robberuda <robberuda@gmail.com>
2021-08-17 09:54:57 +0200

    animation ready

--- animation.m
+++ animation.m
@@ -1,23 +1,69 @@
-%animation of drone
-d = 0.2;
+y_plot = P(:,1);
+z_plot = P(:,2);
+roll_plot = P(:,3);
+t_plot = P(:,4);
 
-i=1;
-for t=1:tss:Tfinal;  
-    x = P(i,1);
-    y = P(i,2);
+figure;
+curve = animatedline('LineWidth',0.5);
+curver = animatedline('LineWidth',0.1,'LineStyle',':');
+curvel = animatedline('LineWidth',0.1,'LineStyle',':');
+
+set(gca, 'XLim', [-1.0,1.0],'YLim',[-5 5],'ZLim',[-5 5]);
+view(90,0)
+hold on;
+grid on;
+
+x_plot = 0*t_plot
+
+bar = 1;
+
+yr_plot = y_plot + bar*cos(roll_plot)
+yl_plot = y_plot - bar*cos(roll_plot)
+zr_plot = z_plot - bar*sin(roll_plot)
+zl_plot = z_plot + bar*sin(roll_plot)
+
+n = length(t_plot)
+for i = 1:n
+    addpoints(curve, x_plot(i), y_plot(i), z_plot(i));
+    addpoints(curver, x_plot(i), yr_plot(i), zr_plot(i));
+    addpoints(curvel, x_plot(i), yl_plot(i), zl_plot(i));
+    head = scatter3(x_plot(i), y_plot(i), z_plot(i),'filled','MarkerFaceColor','b','MarkerEdgeColor','b')
+    headr = scatter3(x_plot(i), yr_plot(i), zr_plot(i),'filled','MarkerFaceColor','r','MarkerEdgeColor','r')
+    headl = scatter3(x_plot(i), yl_plot(i), zl_plot(i),'filled','MarkerFaceColor','r','MarkerEdgeColor','r')
+
+    % Their vertial concatenation is what you want
+    pts = [ [0,yl_plot(i),zl_plot(i)]; [0,yr_plot(i),zr_plot(i)] ];
+
+    % Alternatively, you could use plot3:
+    frame = plot3(pts(:,1), pts(:,2), pts(:,3),'b');
     
-    d1x = x - cos(P(i,3));
-    d1y = y - sin(P(i,3));
-    d2x = x + cos(P(i,3));
-    d2y = y + sin(P(i,3));
+    drawnow;
+    %pause(ts);
     
-    plot([d1x d1y],[d2x d2y],P(:,1),P(:,2),x,y,'s','MarkerSize',15,'MarkerFaceColor','R','MarkerEdgeColor','R');
-    %plot([d1x d1y],[d2x d2y]);
-    ylim([-5,5]);
-    xlim([-5,5]);
-    anim(i)=getframe;
-    i=i+1;
+    delete(head);
+    delete(headr);
+    delete(headl);
+    delete(frame);
+    
 end
 
+clear n;
+clear pts;
+clear curve;
+clear curver;
+clear curvel;
+
+clear x_plot;
+clear y_plot;
+clear yl_plot;
+clear yr_plot;
+clear z_plot;
+clear zl_plot;
+clear zr_plot;
+clear roll_plot;
 clear i;
-    
+clear ilist;
+clear frame;
+clear head;
+clear headl;
+clear headr;
