a4a3c0c4952f8f56a6eb90c2ec699884189f08f3
Augusto-Mura <62520759+Augusto-Mura@users.noreply.github.com>
2021-12-16 12:18:33 +0100

    Esperimenti con transfer function e model linearizer

---
+++ Esperimenti_droni_2d/drone_2d_analitico.m
@@ -0,0 +1,35 @@
+% parametri della discretizzazione
+Tcamp = 0.01;
+method = 'tustin';
+% FINE parametri della discretizzazione
+
+% coefficienti PID
+Pz = 0.65;
+Iz = 0.3;
+Dz = 0.35;
+Nz = 100;
+% FINE coefficienti PID
+
+PID_DT_Z = coomputeDT_PID_TF(Pz, Iz, Dz, Nz, Tc);
+
+% ------ PID discreto from scratch --------
+function fdtPIDZDT = coomputeDT_PID_TF(P, I, D, N, Ts)
+    fdtPZNumDT = P;
+    fdtPZDenDT = 1;
+
+    fdtIZNumDT = I*Ts;
+    fdtIzdenDT = [1 -1];
+
+    fdtDZNumDT = D*N;
+    fdtDZDenNumDT = 1;
+    fdtDZDenDenDT = [1, -1]; % questo mi da 1/(z-1)
+    fdtDZDenDT = 1+N*Ts*tf(fdtDZDenNumDT, fdtDZDenDenDT, Ts);
+
+    fdtPZDT = tf(fdtPZNumDT, fdtPZDenDT, Ts);
+    fdtIZDT = tf(fdtIZNumDT, fdtIzdenDT, Ts);
+    fdtDZDT = fdtDZNumDT/fdtDZDenDT;
+
+    fdtPIDZDT = fdtPZDT + fdtIZDT + fdtDZDT;
+end
+% ------ FINE PID discreto from scratch --------
+
